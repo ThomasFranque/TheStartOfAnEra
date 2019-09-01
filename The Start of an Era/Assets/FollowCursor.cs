5d9fefaeb386845c760c7ded767cc2c5e284d1c2
Thomas <tfranque@gmail.com>
2019-09-02 00:07:09 +0100

    Rough Test of toroidal movement

---
+++ The Start of an Era/Assets/FollowCursor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 mousePos = Input.mousePosition;
+		Vector2 wantedPos = Camera.main.ScreenToWorldPoint(
+			new Vector2(mousePos.x, mousePos.y));
+		transform.position = wantedPos;
+	}
+}
---
+++ The Start of an Era/Assets/Toroidal.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toroidal : MonoBehaviour
+{
+	private BoxCollider2D boxCol;
+	private Vector2 baseColSize;
+	private float horizontalMin, horizontalMax, verticalMin, verticalMax;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		boxCol = gameObject.GetComponent<BoxCollider2D>();
+
+		Camera camera = Camera.main;
+		float sHeight = camera.orthographicSize;
+		float sWidth = camera.aspect * sHeight;
+
+		horizontalMin = -sWidth;
+		horizontalMax = sWidth;
+
+		verticalMin = -sHeight;
+		verticalMax = sHeight;
+
+		baseColSize = boxCol.size;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		// Debug.Log("vertical out");
+
+		if (transform.position.y + baseColSize.y >= verticalMax)
+		{
+			boxCol.size = new Vector2(boxCol.size.x, baseColSize.y - (transform.position.y + baseColSize.y - verticalMax));
+			Debug.Log("vertical out");
+		}
+
+		if (transform.position.x > horizontalMax || transform.position.x < horizontalMin)
+		{
+
+			Debug.Log("Horiz out");
+		}
+	}
+}
