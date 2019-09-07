f2f7a35873d75a76a8b61c2225df6b794d23298a
Miguel Romão Fernández <miguelrf95@gmail.com>
2019-09-07 19:12:16 +0100

    Create new scripts for entities, player script add. Organized script on folders

---
+++ The Start of an Era/Assets/Scripts/EntityControl/Entity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    // Entity variables
+    public Rigidbody2D rb;
+    public Vector2 movement;
+
+    // Properties//
+    // Move speed
+    [SerializeField] public float MaxSpeed { get; set; }
+
+    public virtual void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public virtual void Update()
+    {
+
+    }
+
+    public virtual void Move()
+    {
+        movement = rb.velocity;
+
+        movement = new Vector2(MaxSpeed, 0);
+
+    }
+}
---
+++ The Start of an Era/Assets/Scripts/EntityControl/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    // Properties//
+    // Jump height
+    [SerializeField] private float Height { get; set; }
+
+    // Check if player is on the ground
+    private bool IsGrounded
+    {
+        get
+        {
+            Collider2D collider = Physics2D.OverlapCircle(
+                transform.position, 2.0f, LayerMask.GetMask("Ground"));
+            return (collider != null);
+        }
+    }
+
+    public override void Update()
+    {
+        Move();
+    }
+
+    // Method for player movement
+    public override void Move()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        movement = rb.velocity;
+
+        movement = new Vector2(hAxis * MaxSpeed, movement.y);
+
+        // Make player face the right direction
+        float shouldInvert = hAxis * transform.right.x;
+        if (shouldInvert < 0.0f)
+        {
+            float yAngle = 180.0f;
+
+            transform.rotation =
+                transform.rotation * Quaternion.Euler(0.0f, yAngle, 0.0f);
+        }
+
+        Jump();
+
+        rb.velocity = movement;
+    }
+
+    // Method for jumping
+    private void Jump()
+    {
+        // Jump only if player is on the ground
+        if (Input.GetKeyDown(KeyCode.Space))
+            if (IsGrounded)
+            {
+                movement.y = Height;
+            }
+    }
+
+}
