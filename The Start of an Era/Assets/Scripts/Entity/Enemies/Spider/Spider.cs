f8318b5e2fffb81a77a0f488f975ed5960e9228c
Thomas the Franque <tfranque@gmail.com>
2019-09-08 01:16:55 +0100

    Add Enemy script base, methods to Entity and Player and new Scene

---
+++ The Start of an Era/Assets/Scripts/Entity/Enemies/Spider/Spider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : Enemy
+{
+	public override int HP { get; protected set; }
+
+	// Start is called before the first frame update
+	protected override void Start()
+	{
+		base.Start();
+	}
+
+	// Update is called once per frame
+	protected override void Update()
+	{
+		base.Update();
+	}
+
+	protected override void Move()
+	{
+		// Blablabla playerScript.position move to that, kill etc
+	}
+
+	protected override void WhileTargetingPlayer()
+	{
+		Debug.Log("Wtf it works");
+	}
+
+	protected override void OnHit(int damage)
+	{
+		HP -= damage;
+	}
+}
---
+++ The Start of an Era/Assets/Scripts/Entity/Entity.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public abstract class Entity : MonoBehaviour
+{
+	[Header ("--- Entity Properties ---")]
+	// Entity variables
+	[SerializeField] public float maxSpeed;
+	protected Vector3 colliderOffset;
+	protected Vector2 movement;
+	protected Rigidbody2D rb;
+
+    // Properties//
+	public abstract int HP { get; protected set; }
+
+    // Check if player is on the ground
+    public bool IsGrounded
+    {
+        get
+        {
+            Collider2D collider = Physics2D.OverlapCircle(
+                transform.position + colliderOffset, 2.0f, LayerMask.GetMask("Ground"));
+            return (collider != null);
+        }
+    }
+
+    protected virtual void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+	public void Hit(int damage)
+	{
+		OnHit(damage);
+	}
+
+	protected abstract void Move();
+	//movement = rb.velocity;
+	//movement = new Vector2(MaxSpeed, 0);
+
+	protected abstract void OnHit(int damage);
+}
