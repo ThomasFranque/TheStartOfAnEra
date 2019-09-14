3447b489ca67c5d8bbea501063822849b6505246
Thomas the Franque <tfranque@gmail.com>
2019-09-14 01:39:44 +0100

    Added random move direction to the spider and messed with its footsteps sound

--- The Start of an Era/Assets/Scripts/Entity/Enemies/Spider/Spider.cs
+++ The Start of an Era/Assets/Scripts/Entity/Enemies/Spider/Spider.cs
@@ -6,12 +6,14 @@ public class Spider : Enemy
 {
 	public override int HP { get; protected set; }
 
-    // ** Sound
-    [Header("SoundFX")]
-    public AudioSource walk;
+	// ** Sound
+	[Header("SoundFX")]
+	public AudioSource walk;
 
 	private float idleStopTime, idleWalkTime, idleWalkSpeed;
 	private float timeOfIdleStop, timeOfIdleWalk;
+
+	private int walkDirection;
 
 	private bool IsIdle
 	{
@@ -28,19 +30,17 @@ public class Spider : Enemy
 		timeOfIdleStop = 3.0f;
 		timeOfIdleWalk = 5.0f;
 
-        walk = GetComponent<AudioSource>();
-
 		maxSpeed = 100;
 		HP = 1;
 	}
 
-    // Update is called once per frame
-    protected override void Update()
+	// Update is called once per frame
+	protected override void Update()
 	{
 		base.Update();
-    }
+	}
 
-    protected override void Move()
+	protected override void Move()
 	{
 		// Blablabla playerScript.position move to that, kill etc
 
@@ -48,24 +48,37 @@ public class Spider : Enemy
 		// check if Stopped
 		if (!IsIdle)
 		{
-			// Time it will be standing
 			if (Time.time > idleStopTime + timeOfIdleStop)
 			{
-				// Time it will be walking
+				// This will run Once before starting walking
 				idleStopTime = Time.time + timeOfIdleWalk;
+
+				// Set walking direction
+				walkDirection = Random.Range(0, 101);
+				walkDirection = walkDirection <= 50 ? 1 : -1;
 			}
 			else
 			{
-                // MOVING
-                // Sound
-                StartCoroutine(WalkCoroutine());
+				// Is walking
 
-                movement = rb.velocity;
-				movement = new Vector2(0.5f * maxSpeed, movement.y);
+				////
+				//if (walk.mute)
+				//	StartCoroutine(WalkCoroutine());
+				////
+
+				// My solution, maybe works better?
+				// the sound should maybe loop too, and be shorter?
+				if (!walk.isPlaying)
+				{
+					walk.pitch = Random.Range(0.75f, 1.5f);
+					walk.Play();
+				}
+
+				movement = rb.velocity;
+				movement = new Vector2(0.5f * maxSpeed * walkDirection, movement.y);
 				rb.velocity = movement;
 			}
 		}
-		//Debug.Log($"Time: {Time.time}");
 		#endregion
 	}
 
@@ -94,16 +107,16 @@ public class Spider : Enemy
 		throw new System.NotImplementedException();
 	}
 
-    internal IEnumerator WalkCoroutine()
-    {
-        walk.mute = false;
-        walk.pitch = Random.Range(0.75f, 1.5f);
-        yield return new WaitForSeconds(0.3f);
-        walk.mute = true;
-    }
+	//   internal IEnumerator WalkCoroutine()
+	//   {
+	//       walk.mute = false;
+	//       walk.pitch = Random.Range(0.75f, 1.5f);
+	//       yield return new WaitForSeconds(timeOfIdleWalk);
+	//	walk.mute = true;
+	//}
 
 #if UNITY_EDITOR
-    private void OnDrawGizmos()
+	private void OnDrawGizmos()
 	{
 		Gizmos.color = Color.yellow;
 		Gizmos.DrawSphere(transform.position, 2.0f);
